d81a3e41843375b3ea931ea0d6a850c29a88da2b
Somchai Somphadung <somchaisomph@gmail.com>
2018-07-29 18:10:40 +0700

    Update reinhard.py

--- color_transfer/reinhard.py
+++ color_transfer/reinhard.py
@@ -1,4 +1,8 @@
-
+'''
+Erik Reinhard, Michael Ashikhmin, Bruce Gooch and Peter Shirley,
+'Color Transfer between Images', IEEE CG&A special issue on Applied
+Perception, Vol 21, No 5, pp 34-41, September - October 2001
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage.io
